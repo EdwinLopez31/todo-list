d378e8fd6087436ee9fd99cb7164f5ebf40bbe0a
Edwin Lopez <ednlopezjr@gmail.com>
2023-03-22 08:49:53 +0800

    feat: added example for test

---
+++ app/homepage.test.tsx
@@ -0,0 +1,13 @@
+import { expect, it, describe } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import Home from './page';
+
+describe('home', () => {
+  it('should have a h1 stating my next js boilerplate', () => {
+    render(<Home />);
+    const main = within(screen.getByRole('main'));
+    expect(
+      main.getByRole('heading', { level: 1, name: /My NextJS Boilerplate/i })
+    ).toBeDefined();
+  });
+});
